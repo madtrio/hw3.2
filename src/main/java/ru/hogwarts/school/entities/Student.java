3c211f5a05331acc16ef12bb1a108b6d91d1626f
madtrio <anna.kozitzyna@yandex.ru>
2023-07-16 12:54:42 +0300

    2

---
+++ src/main/java/ru/hogwarts/school/entities/Student.java
@@ -0,0 +1,65 @@
+package ru.hogwarts.school.entities;
+
+import java.util.Objects;
+
+public class Student {
+
+    private long id;
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student (long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Students{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student students = (Student) o;
+        return id == students.id && age == students.age && Objects.equals(name, students.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+}
